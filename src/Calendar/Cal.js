d84ac6e904964f5bb6169ad3044870c3bcb29a3f
YiRen <yiren987@gmail.com>
2022-11-11 19:51:37 -0500

    calendar

---
+++ src/Calendar/Cal.js
@@ -0,0 +1,57 @@
+import {Calendar, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import React, {useState} from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css"
+import "../css/calendar.css"
+
+const locales = {
+    "en-US": require("date-fns/locale/en-US")
+}
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events = [
+    {
+        title:"ctp presentation",
+        allDay:true,
+        start: new Date(2022,10,4),
+        end: new Date(2022,10,4)
+    },
+]
+function Cal(){
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [allEvents, setAllEvents] = useState(events);
+
+    function handleAddEvent() {
+         
+        setAllEvents([...allEvents, newEvent]);
+    }
+
+    return (
+        <div className="App">
+            <h1>Calendar</h1>
+            <h2>Add New Event</h2>
+            <div>
+                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                    Add Event
+                </button>
+            </div>
+            <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+        </div>
+    );
+}
+
+export default Cal;
